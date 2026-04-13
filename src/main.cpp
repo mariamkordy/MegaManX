0eb0fd7889ac3304fcf25ec9aea89f7848634c22
Mariam <mariamkordyy@gmail.com>
2026-04-13 03:12:52 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include "Player.h"
 
 int main()
 {
+    Player myplayer;
+    myplayer.circle.setRadius(50.f);
+    myplayer.circle.setPosition(100.f, 100.f);
+
+    sf::RectangleShape ground(sf::Vector2f(800.f, 100.f));
+    ground.setPosition(0.f, 550.f);
+
     sf::RenderWindow window(sf::VideoMode(800, 600), "MegaManX");
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        float deltaTime = clock.restart().asSeconds();
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -13,7 +24,18 @@ int main()
                 window.close();
         }
 
+        player_movement(myplayer, deltaTime);
+        jump(myplayer);
+        gravity(myplayer, deltaTime);
+        checkGround(myplayer, ground);
+        playerpos(myplayer);
+        collision_with_frame(myplayer);
+
         window.clear(sf::Color::Black);
+
+        window.draw(myplayer.circle);
+        window.draw(ground);
+
         window.display();
     }
 
